c9013a5bf90ae96e1352dc3a2cf428713de92211
unknown <venugopalsp.64@gmail.com>
2026-01-19 17:25:04 +0530

    changed homepage

--- src/data/initialBoard.js
+++ src/data/initialBoard.js
@@ -1,4 +1,4 @@
 export const initialBoard = [
   { id: "1", text: "Drag me!", x: 100, y: 120, color: "#7da4e8" },
-  { id: "2", text: "Edit me!", x: 150, y: 180, color: "#e8af7d" },
+  { id: "2", text: "Edit me!", x: 150, y: 240, color: "#e8af7d" },
 ];
